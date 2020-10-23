8de3e7935a5b27e7c26d4f73dd413f7593a0287e
mayorau3 <mayo1129rau1206@gmail.com>
2020-10-23 12:08:23 +0900

    ストロングパラメーター、バリデーション設定、ビュー調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_family_name, :kanji_first_name, :zip_code, :region_id, :city, :address1, :address2, :phone])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+
+  # パスワード半角英数字混合
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  # 名前漢字・平仮名・カタカナ
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :kanji_family_name
+    validates :kanji_first_name
+  end
+
+  with_options presence: true do
+    validates :city, :address1
+    # 「住所」の郵便番号に関するバリデーション
+    validates :zip_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+    # 「電話番号」に関するバリデーション
+    validates :phone, length: { maximum: 11 }
+  end
+    # 「住所」の都道府県に関するバリデーション
+    # validates :reigon_id, numericality: { other_than: 1, message: 'Select' }
 
   has_one :house
   has_many :cats
