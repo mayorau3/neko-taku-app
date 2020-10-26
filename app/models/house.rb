1824b766c4120e188d9a97f37a78be1cfc7b54e8
mayorau3 <mayo1129rau1206@gmail.com>
2020-10-26 16:51:34 +0900

    価格のバリデーション設定

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,11 +1,18 @@
 class House < ApplicationRecord
   belongs_to :user
   has_many :reservations
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :cat_history
 
   validates :pr, :explanation, presence: true
+
+  # stay_priceが空ならば、one_day_priceを必須にする
+  validates :one_day_price, presence: true, unless: :stay_price?
+
+  # one_day_priceが空ならば、stay_priceを必須にする
+  validates :stay_price, presence: true, unless: :one_day_price?
+
   # 「猫歴」に関するバリデーション 選択が「---」の時は保存できない
   validates :cat_history_id, numericality: { other_than: 1, message: 'Select' }
 end
