5b4dd9f065a15f0e054778357523e44022234cbb
mayorau3 <mayo1129rau1206@gmail.com>
2020-10-26 10:29:36 +0900

    ActiveHash猫歴用モデル作成、アソシエーション

---
+++ app/models/cat_history.rb
@@ -0,0 +1,11 @@
+class CatHistory < ApplicationRecord
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '1年未満' },
+    { id: 3, name: '1年以上' },
+    { id: 4, name: '3年以上' },
+    { id: 5, name: '5年以上' },
+    { id: 6, name: '10年以上' },
+    { id: 7, name: 'なし' },
+  ]
+end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,5 @@
 class House < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  belongs_to_active_hash :cat_history
 end
