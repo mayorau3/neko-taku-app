a2544b816765d659d7faeb9cf112da0c2abea0f3
mayorau3 <mayo1129rau1206@gmail.com>
2020-10-28 11:39:50 +0900

    エラーメッセージ表示、価格バリデーション再設定

--- app/models/house.rb
+++ app/models/house.rb
@@ -8,15 +8,18 @@ class House < ApplicationRecord
 
   validates :pr, :explanation, presence: true
 
-  # 価格の範囲指定 ¥300〜¥9,999,999の間、半角数字のみ可
-  validates :one_day_price, :stay_price, presence: true, format: { with: /\A.[0-9]+\z/, message: 'Half-width number' }
-
-  # stay_priceが空ならば、one_day_priceを必須にする
-  validates :one_day_price, presence: true, unless: :stay_price?
-
-  # one_day_priceが空ならば、stay_priceを必須にする
-  validates :stay_price, presence: true, unless: :one_day_price?
-
   # 「猫歴」に関するバリデーション 選択が「---」の時は保存できない
   validates :cat_history_id, numericality: { other_than: 1, message: 'Select' }
+
+  # 価格 日帰り、お泊まり、どちらか片方入力必須にする
+  validates :stay_or_one_day, presence: true
+  # 価格 半角数字のみ可
+  validates :one_day_price, format: { with: /\A.[0-9]+\z/, message: 'Half-width number' }, unless: ->{ one_day_price.blank?}
+  validates :stay_price, format: { with: /\A.[0-9]+\z/, message: 'Half-width number' }, unless: ->{ stay_price.blank?}
+
+  private
+    def stay_or_one_day
+      stay_price.presence or one_day_price.presence
+    end
+
 end
