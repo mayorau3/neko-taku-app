3212c6984a24a5536d03906d2879353a42e4ba66
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-04 16:19:52 +0900

    画像登録、プレビュー機能実装

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,7 @@
 class Cat < ApplicationRecord
   belongs_to :user
   has_many :reservations
+ # has_many_attached :images
 
   validates :name, :age_id, :sex, :explanation, :images, presence: true
 
