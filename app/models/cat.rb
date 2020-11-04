b9b60064e782e615e4230ee458e77131043df991
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-04 14:53:43 +0900

    バリデーション設定

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,9 @@
 class Cat < ApplicationRecord
   belongs_to :user
   has_many :reservations
+
+  validates :name, :age_id, :sex, :explanation, :images, presence: true
+
+  # 「年齢」に関するバリデーション 選択が「---」の時は保存できない
+  validates :age_id, numericality: { other_than: 1, message: 'Select' }
 end
