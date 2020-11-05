19c6c6b174a8e13d83d80045b7d87218242d6caf
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-05 12:39:40 +0900

    コントローラー、ストロングパラメーター設定

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,21 @@
 class ReservationsController < ApplicationController
 
   def new
-  
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :stay_days, :one_day_hours, :message).merge(user_id: current_user.id, house_id: params[:house_id], cat_id: params[:cat_id])
   end
 end
