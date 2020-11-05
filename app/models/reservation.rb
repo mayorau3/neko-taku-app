ab24cfb9c2330f0c636cae9661178b07547328fe
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-05 16:33:22 +0900

    バリデーション設定

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,14 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :house
   belongs_to :cat
+
+  validates :start_date, :message, presence: true
+
+  # 価格 日帰り、お泊まり、どちらか片方入力必須にする
+  validates :stay_or_one_day, presence: true
+
+  private
+  def stay_or_one_day
+    stay_days.presence or one_day_hours.presence
+  end
 end
