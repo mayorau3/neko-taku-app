793610e72500426570fb3c9c5ceabbb01f733e1f
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-06 17:08:09 +0900

    house,catデータ取得

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,9 @@
 class ReservationsController < ApplicationController
+  before_action :set_house
+  before_action :set_cat
 
   def new
     @reservation = Reservation.new
-    @cat = Cat.find(current_user.id)
   end
 
   def create
@@ -18,5 +19,13 @@ class ReservationsController < ApplicationController
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :stay_days, :one_day_hours, :message).merge(user_id: current_user.id, house_id: params[:house_id], cat_id: params[:cat_id])
+  end
+
+  def set_house
+    @house = House.find(params[:house_id])
+  end
+
+  def set_cat
+    @cat = Cat.find(current_user.id)
   end
 end
