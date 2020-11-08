da78f80eb67d4a2c0d6813f18b91eb101797bb1d
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-08 15:30:34 +0900

    reservationモデル単体テスト

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  describe '#create' do
+    before do
+      @reservation = FactoryBot.build(:reservation)
+    end
+
+    it '全ての入力項目が存在すれば登録できること' do
+      expect(@reservation).to be_valid
+    end
+
+    it 'お預け日が空では登録できないこと' do
+      @reservation.start_date = nil
+      @reservation.valid?
+      expect(@reservation.errors.full_messages).to include("Start date can't be blank")
+    end
+
+    it '日帰り滞在時間、お泊まり日数が両方とも空では登録できないこと' do
+      @reservation.one_day_hours = nil
+      @reservation.stay_days = nil
+      @reservation.valid?
+      expect(@reservation.errors.full_messages).to include("Stay or one day can't be blank")
+    end
+
+    it '日帰り滞在時間が空でもお泊まり日数に入力があれば登録できること' do
+      @reservation.one_day_hours = nil
+      @reservation.stay_days = '4'
+      expect(@reservation).to be_valid
+    end
+
+    it 'お泊まり日数が空でも日帰り滞在時間に入力があれば登録できること' do
+      @reservation.one_day_hours = '5'
+      @reservation.stay_days = nil
+      expect(@reservation).to be_valid
+    end
+
+    it 'メッセージが空では登録できないこと' do
+      @reservation.message = nil
+      @reservation.valid?
+      expect(@reservation.errors.full_messages).to include("Message can't be blank")
+    end
+
+  end
+end
