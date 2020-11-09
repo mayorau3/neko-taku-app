b21e8dde94d39e2843b688f7858cb5c69901dcb5
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-09 12:37:32 +0900

    バリデーション追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,11 +4,11 @@ class Reservation < ApplicationRecord
 
   validates :start_date, :message, presence: true
 
-  # 価格 日帰り、お泊まり、どちらか片方入力必須にする
-  validates :stay_or_one_day, presence: true
+  # 日帰り滞在時間、お泊まり日数、どちらか片方だけ入力必須にする 両方はダメ
+  validates :either_stay_or_one_day, presence: { message: ' / Only one of them' }
 
   private
-  def stay_or_one_day
-    stay_days.presence or one_day_hours.presence
+  def either_stay_or_one_day
+    stay_days.present? ^ one_day_hours.present?
   end
 end
