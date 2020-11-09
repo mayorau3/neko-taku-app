390132da10107b4f21f92024caf4b87a8d28453b
mayorau3 <mayo1129rau1206@gmail.com>
2020-11-09 15:57:44 +0900

    猫年齢表記変更

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,12 +1,12 @@
 class Age < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '6ヶ月' },
-    { id: 3, name: '1歳' },
-    { id: 4, name: '1歳半' },
-    { id: 5, name: '2歳以上' },
-    { id: 6, name: '5歳以上' },
-    { id: 7, name: '10歳以上' },
+    { id: 2, name: '約6ヶ月' },
+    { id: 3, name: '約1歳' },
+    { id: 4, name: '約1歳半' },
+    { id: 5, name: '2歳〜5歳' },
+    { id: 6, name: '5歳〜10歳' },
+    { id: 7, name: '10歳〜15歳' },
     { id: 8, name: '15歳以上' },
   ]
 end
